e835c52847c0c40c2f8e0d946ee06338585c6d89
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-09 17:34:19 +0200

    error and exception handling

--- backend/contrrollers/goalController.js
+++ backend/contrrollers/goalController.js
@@ -1,29 +1,32 @@
 //@desc set goals
 //@route GET /api/goals
 //@access private
-const getGoals = (req,res) => {
-    console.log(req.body);
+const getGoals =  (req,res) => {
+    if(!req.body.text){
+        res.status(404)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({ message: "Get goals" });
 }
 
 //@desc create goals
 //@route  post/api/goals
 //@access private
-const createGoals=(req,res) => {  
+const createGoals=  (req,res) => {  
     res.status(200).json({ message: "Create goals" });
 }
 
 //@desc update goal
 //@route PUT /api/goals:id
 //@access private
-const updateGoals=(req,res) => { 
+const updateGoals=  (req,res) => { 
     res.status(200).json({ message: `Update goal ${req.params.id}` });
 }
 
 //@desc delete goal
 //@route DELETE /api/goals:id
 //@access private
-const deleteGoals=(req,res) => {
+const deleteGoals=  (req,res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}` });
 }
 
