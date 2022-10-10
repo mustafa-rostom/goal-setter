dd1159c197ece1e6b6219cad64518ec9f11ffec2
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-10 18:04:41 +0200

    add user model and add it to goal model

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add a email address'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+    }
+},{
+    timestamps: {
+        createdAt: true,
+}})
+
+module.exports = mongoose.model('User', userSchema);
+
