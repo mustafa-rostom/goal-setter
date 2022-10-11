d4672a970c4b64d776729516d607c4899aeeca5a
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-11 14:29:23 +0200

    generate JWT

--- backend/contrrollers/userController.js
+++ backend/contrrollers/userController.js
@@ -35,7 +35,8 @@ const regesterUser = asynchandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token:generateToken(user._id)
         })
     }
     else {
@@ -57,7 +58,8 @@ const loginUser = asynchandler(async (req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(401);
@@ -71,7 +73,12 @@ const getMe = asynchandler(async (req, res) => {
     res.json({ message: 'Regester user' })
 })
 
-
+// Generate JWT
+const generateToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn: '30d'
+    })
+}
 module.exports = {
     regesterUser,
     loginUser,
