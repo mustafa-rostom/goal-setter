d77b25d984091af2b12370b4f1c5c2d187c47200
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-11 21:11:19 +0200

    setting ,update and delete user goal

--- backend/contrrollers/goalController.js
+++ backend/contrrollers/goalController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
-const Goal=require('../models/goalModel');
+const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 //@desc set goals
 //@route GET /api/goals
 //@access private
@@ -17,7 +18,8 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user:req.user.id
     })
     
     res.status(200).json(goal);
@@ -31,6 +33,21 @@ const updateGoals = asyncHandler(async (req, res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+
+    const user =await User.findById(req.user.id)
+    
+    //Check for user
+    if(!user){
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    //Make sure the logged in user matches the goal user
+    if(goal.user.toString()!== user.id){
+        res.status(401)
+        throw new Error('User not authorized') 
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{new:true})
@@ -47,8 +64,23 @@ const deleteGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    //Check for user
+    if (!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+
+    //Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     await goal.remove();
-    res.status(200).json({ id: require.params.id});
+    res.status(200).json({ id: req.params.id});
 })
 
 module.exports = {
