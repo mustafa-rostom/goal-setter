71e7f13c34d1837b2c2fd07e372d54a8657fdcd8
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-12 15:51:57 +0200

    Header & Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
-    <div>
-      <h1>
-        MyApp
-      </h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
