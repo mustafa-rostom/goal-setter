302464df87f9907a99037887544b1aa5d1d66fbe
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-13 19:41:26 +0200

    Redux start & Auth slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../featuers/auth/authSlice'
 
 export const store = configureStore({
   reducer: {
+    auth : authReducer
   },
 });
