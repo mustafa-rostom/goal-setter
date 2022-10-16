757d6b397a42657973e66bef61d828106c18d65d
Mustafa Rostom <mutafarostom@gmail.com>
2022-10-16 22:39:04 +0200

    add user registration

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+
+function Spinner() {
+  return (
+    <div className="loadingSpinnerContainer">
+        <div className="laodingSpinner"></div>
+    </div>
+  )
+}
+
+export default Spinner
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,46 +1,61 @@
-import { useState, useEffect } from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 function Login() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    })
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const { email, password } = formData;
+  const { email, password } = formData;
 
-    const onChange=(e)=>{
-        setFormData((prevState)=>({
-            ...prevState,
-            [e.target.name]:e.target.value
-        }))
-    }
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    const onSubmit=(e)=>{
-        e.preventDefault();
-    }
-    return (
-        <>
-            <section className='heading'>
-                <h1>
-                    <FaSignInAlt />  Login
-                </h1>
-                <p>Login and start setting goals </p>
-            </section>
-            <section className='form'>
-                <form onSubmit={onSubmit}>
-                    <div className="form-group">
-                        <input type="email" id='email' name='email' value={email} onChange={onChange} placeholder='Enter your email' />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" id='password' name='password' value={password} onChange={onChange} placeholder='Enter your password' />
-                    </div>
-                    <button type="submit" className='btn btn-block'>Login</button>
-                </form>
-            </section>
-        </>
-
-    )
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals </p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter your password"
+            />
+          </div>
+          <button type="submit" className="btn btn-block">
+            Login
+          </button>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
